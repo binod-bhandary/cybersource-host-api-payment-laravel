71714eafe81830865314d56da4853d829f45094e
Binod Bhandary <binod.bhandary@nicaisabank.com>
2023-12-19 12:29:56 +0545

    update

--- config/logging.php
+++ config/logging.php
@@ -18,7 +18,7 @@ return [
     |
     */
 
-    'default' => env('LOG_CHANNEL', 'daily'),
+    'default' => env('LOG_CHANNEL', 'stack'),
 
     /*
     |--------------------------------------------------------------------------
@@ -54,7 +54,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['daily'],
             'ignore_exceptions' => false,
         ],
 
